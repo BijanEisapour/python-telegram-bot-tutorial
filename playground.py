77f6a8e4c6471507bee862a80e3847df59576ea7
BijanEisapour <bijanprogrammer@gmail.com>
2021-10-21 17:29:12 +0330

    [ADD] python basics

--- playground.py
+++ playground.py
@@ -1,1 +1,25 @@
-print("Hello, friend!")
+x = 2
+y = 3
+
+result = x + y
+
+print(result)
+print("the final result is " + str(result))
+print(f"the final result is {result}")
+
+print("\n" + 30 * "=" + "\n")
+
+
+def some_function_name(first_number, second_number):
+    print("in some_function_name")
+    return first_number + second_number
+
+
+def some_other_function_name(first_number: int, second_number: int) -> int:
+    print("in some_other_function_name")
+    return first_number + second_number
+
+
+print(f"result of some_function_name: {some_function_name(4, 8)}")
+print()
+print(f"result of some_other_function_name: {some_other_function_name(15, 16)}")
