01e5e2739398437611d5c08a52c811e14a08d2c8
BijanEisapour <bijanprogrammer@gmail.com>
2021-10-21 17:59:11 +0330

    [ADD] hello message handler

--- playground.py
+++ playground.py
@@ -24,6 +24,27 @@ def about_command_handler(update: telegram.Update, context: telegram.ext.Callbac
     update.message.reply_text("Developed by @BijanProgrammer")
 
 
+def hello_message_handler(update: telegram.Update, context: telegram.ext.CallbackContext) -> None:
+    print(update)
+    print(context)
+    
+    user = update.message.from_user
+    
+    first_name = user.first_name or ''
+    last_name = user.last_name or ''
+    username = user.username or ''
+    
+    if first_name != '':
+        if last_name != '':
+            update.message.reply_text(f"Hello, {first_name} {last_name}!")
+        else:
+            update.message.reply_text(f"Hello, {first_name}!")
+    elif username != '':
+        update.message.reply_text(f"Hello, {username}!")
+    else:
+        update.message.reply_text("Hello, friend!")
+
+
 def main():
     print("initializing updater ...")
     updater = telegram.ext.Updater(TOKEN, use_context=True)
@@ -35,6 +56,9 @@ def main():
     dispatcher.add_handler(telegram.ext.CommandHandler("start", start_command_handler))
     dispatcher.add_handler(telegram.ext.CommandHandler("help", help_command_handler))
     dispatcher.add_handler(telegram.ext.CommandHandler("about", about_command_handler))
+    
+    print("initializing message handlers ...")
+    dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, hello_message_handler))
     
     print("starting poll ...")
     updater.start_polling()
